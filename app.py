0ee847a32649293b8e2d929871ff296302cdddf3
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-01-03 18:24:47 +0300

    send station dynamically

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000  # One year in seconds
 
+station = "Club Country - Muthiga Inn"
+
 # Routes
 @app.route('/')
 def index():
@@ -15,18 +17,20 @@ def index():
 
 @app.route('/login', methods=['GET'])
 def login():      
+    
     link_login_only = "http://192.168.88.1"
     username = "GHDRSD"
     password = "FGSDVAT%^S"
     videos = ["vid_0.mp4","vid_1.mp4","vid_2.mp4"]
     images = ['img_0.jpg', 'img_1.jpg', 'img_2.jpg', 'img_3.jpg', 'img_4.jpg', 'img_5.jpg', 'img_6.jpg', 'img_7.jpg', 'img_8.jpg', 'img_9.jpg']
-    return render_template('login.html', link_login_only=link_login_only, username=username, password=password,
+    return render_template('login.html', station=station, link_login_only=link_login_only, 
+                           username=username, password=password,
                            video=random.sample(videos, 1)[0], images=random.sample(images, 5))
 
 @app.route('/terms', methods=['GET'])
 def terms():      
-    return render_template('terms.html')
+    return render_template('terms.html', station=station)
 
 if __name__ == '__main__':
-    debug_mode = os.getenv('IS_DEBUG', 'False') in ['True', '1', 't']
+    debug_mode = os.getenv('IS_DEBUG', 'False') in ['True', 'T', 't', '1']
     app.run(debug=debug_mode)
