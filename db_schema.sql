da0e73872a1353efa19f69cc2aebf121bf4c09cd
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-01-06 12:24:01 +0300

    use youtube API

--- db_schema.sql
+++ db_schema.sql
@@ -1,37 +1,50 @@
--- Table structure for table clients
+-- Schema for table clients
 CREATE TABLE IF NOT EXISTS clients (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  youtubeChannelHandle TEXT,
+  backgroundColor TEXT,
+  foregroundColor TEXT,
   createdAt TIMESTAMP
 );
 
--- Table structure for table stations
+-- Schema for table users
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  phone TEXT,
+  password TEXT,
+  createdAt TIMESTAMP,
+  clientId INT,
+  UNIQUE (phone)
+);
+
+-- Schema for table stations
 CREATE TABLE IF NOT EXISTS stations (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  routerIP TEXT,
   hotspotUsername TEXT,
   hotspotPassword TEXT,
-  youtubeChannelHandle TEXT,
   createdAt TIMESTAMP,
   clientId INT
 );
 
--- Table structure for table YouTube Videos
+-- Schema for table YouTube Videos
 CREATE TABLE IF NOT EXISTS youtube_videos (
   id SERIAL PRIMARY KEY,
   videoId TEXT,
+  videoTitle TEXT,
   publishedAt TIMESTAMP,
   clientId INT,
-  stationId INT
+  stationId INT,
+  UNIQUE (videoId, stationId)
 );
 
--- Table structure for table https://i.postimg.cc images
+-- Schema for table https://i.postimg.cc images
 CREATE TABLE IF NOT EXISTS postimg_images (
   id SERIAL PRIMARY KEY,
   imageId TEXT,
   publishedAt TIMESTAMP,
   clientId INT,
-  stationId INT
+  stationId INT,
+  UNIQUE (imageId, stationId)
 );
