3948e56a86b58ccf32bce44f089c1b11e02d5476
Dennis Mutethia Muga <dennismuga@gmail.com>
2026-03-24 11:44:09 +0300

    Update api.py

--- utils/api.py
+++ utils/api.py
@@ -28,7 +28,8 @@ def subscribe(db):
         # Get device info
         user_agent = request.headers.get('User-Agent')
         match = re.search(r'\(([^)]+)\)', user_agent)
-        device = match.group(1) if match else None
+        device_parts = match.group(1).split(';') if match else None
+        device = f"{device_parts[2].strip()}-{device_parts[1].strip()}" if device_parts else 'Unknown Device'
         
         subscriber_id = db.add_subscriber(phone, hotspot_id, hotspot.client_id, device=device)
 
